def6ca87dba928a1856693cbc247baaae26842ac
KenzTranBM <tranhuyhoang.auto@gmail.com>
2024-10-27 02:25:50 +0700

    Cập nhật server.js với tính năng mới

--- server.js
+++ server.js
@@ -83,19 +83,17 @@ app.get('/temp1', (req, res) => {
 });
 app.get('/temp2', (req, res) => {
     const jsonResponse = {
-        key1: "https://t.me/notpixel/app?startapp=f5034969487_t",
-        key2: "627",
-        key3: "935",
-	key4: "29"
+        key1: "https://t.me/notpixel/app?startapp=x16_y16",
+        key2: "https://t.me/notpixel/app?startapp=f1907686444_t",
+        key3: "3"
     };
     res.json(jsonResponse);
 });
 app.get('/temp3', (req, res) => {
     const jsonResponse = {
-        key1: "https://t.me/notpixel/app?startapp=f5315487777_t",
-        key2: "657",
-        key3: "923",
-	key4: "30"
+        key1: "https://t.me/notpixel/app?startapp=x196_y196",
+        key2: "https://t.me/notpixel/app?startapp=f1618637560_t",
+        key3: "11"
     };
     res.json(jsonResponse);
 });
